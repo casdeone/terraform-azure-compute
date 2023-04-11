26bec0edd669830d21e292f4833e85e306c61cd0
Dennis Castillo <dennis.castillo@sharp.com>
2023-04-11 07:24:19 -0700

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "vm_id" {
-    value = {for vm in azurerm_windows_virtual_machine.vm : vm.id => vm.name}
+    value = [for vm in azurerm_windows_virtual_machine.vm : tomap({
+        name = vm.name
+        id = vm.id
+    })]
+}
+
+output "vm_ids" {
+    value = [for vm in azurerm_windows_virtual_machine.vm : tomap({
+        name = vm.name
+        id = vm.id
+    })]
 }
